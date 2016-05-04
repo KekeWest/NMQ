dc6c7b12f926bef2fa5b19ecb745399e5209fe28
KekeWest <KekeWest@users.noreply.github.com>
2016-05-04 13:48:56 +0900

    update

--- src/main/java/org/nmq/NMQChannel.java
+++ src/main/java/org/nmq/NMQChannel.java
@@ -1,34 +1,74 @@
 package org.nmq;
 
+import java.util.List;
+
 import org.nmq.enums.ChannelType;
 
-import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+import lombok.Builder;
 
 public class NMQChannel {
 
-    protected final ChannelConfig config;
-    protected final ServerBootstrap serverBootstrap = null;
-    protected final Bootstrap bootstrap = null;
+	private ChannelType channelType;
+	private String listenTopic;
+	private List<String> topics;
+	private String address;
+	private Integer port;
 
-
-    public NMQChannel(ChannelType channelType, String addr, int port, String... topics) {
-        this.config = createConfig(channelType, addr, port, topics);
-    }
-
-    public NMQChannel(ChannelConfig config) {
-        this.config = config;
-    }
-
-    public ChannelConfig createConfig(ChannelType channelType, String addr, int port, String[] topics) {
-        return null;
+    @Builder
+    public NMQChannel(ChannelType channelType, String listenTopic, List<String> topics,
+    		String address, Integer port) {
+    	this.channelType = channelType;
+    	this.listenTopic = listenTopic;
+    	this.topics = topics;
+    	this.address = address;
+    	this.port = port;
     }
 
     public void start() {
+    	switch (this.channelType) {
+		case PUB:
+		case PUSH:
+			bind();
+			break;
+		case SUB:
+		case PULL:
+			connect();
+			break;
+		default:
+			throw new IllegalArgumentException("Unsupported socket type: " + this.channelType.name());
+		}
+    }
+
+    protected void bind() {
+    	EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+    	EventLoopGroup workerGroup = new NioEventLoopGroup();
+    	 ServerBootstrap serverBootstrap = new ServerBootstrap();
+    	 serverBootstrap.group(bossGroup, workerGroup)
+    	 .channel(NioServerSocketChannel.class)
+    	 .handler(new ChannelInitializer<SocketChannel>() {
+    		 @Override
+    		 public void initChannel(SocketChannel ch) throws Exception {
+    			 ChannelPipeline p = ch.pipeline();
+    			 p.addLast(
+    					 new ObjectEncoder(),
+    					 new ObjectDecoder(ClassResolvers.cacheDisabled(null))
+    					 );
+    		 }
+    	 });
 
     }
 
-    public void close() {
+    protected void connect() {
 
     }
 }
--- src/test/java/org/nmq/LibraryTest.java
+++ src/test/java/org/nmq/LibraryTest.java
@@ -1,11 +1,13 @@
 package org.nmq;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.nmq.enums.ChannelType;
 
 
 public class LibraryTest {
     @Test public void test() {
+    	NMQChannel.builder().channelType(ChannelType.PUB).build();
         assertTrue("someLibraryMethod should return 'true'", true);
     }
 }
